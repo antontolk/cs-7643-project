789e1a38c23c3daf8712f88cb31c7013976f98dd
Anton Tolkachev <atolkachev3@gatech.edu>
2024-10-31 23:50:31 +1300

    Basic project set up and dataset loader.

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+from app.data_load import load_dataset
+from app.settings import Settings
 
 if __name__ == '__main__':
-    pass
+    # Load settings
+    settings = Settings()
+
+
+    # Load dataset
+    df_train, _, _ = load_dataset(
+        train_path=settings.data_load.meld_train,
+        val_path=settings.data_load.meld_val,
+        test_path=settings.data_load.meld_test,
+        dataset='MELD',
+    )
+
+    print(df_train.head(5))
+    print(df_train.dtypes)
