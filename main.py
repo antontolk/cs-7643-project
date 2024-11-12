d881c6ad8f6d2907acb3f1b1408202f96ee58093
Anton Tolkachev <atolkachev3@gatech.edu>
2024-11-13 09:45:52 +1300

    1. Emotion and Sentiment categories replaces by digits. 2. Now data loader returns emotion and sentiment categories, samples and labels for train, validation and test datasets.

--- main.py
+++ main.py
@@ -5,14 +5,19 @@ if __name__ == '__main__':
     # Load settings
     settings = Settings()
 
-
     # Load dataset
-    df_train, _, _ = load_dataset(
-        train_path=settings.data_load.meld_train,
-        val_path=settings.data_load.meld_val,
-        test_path=settings.data_load.meld_test,
-        dataset='MELD',
-    )
+    emotions, sentiments, df_train_x, df_train_y, df_val_x, df_val_y, \
+        df_test_x, df_test_y = load_dataset(
+            train_path=settings.data_load.meld_train,
+            val_path=settings.data_load.meld_val,
+            test_path=settings.data_load.meld_test,
+            dataset='MELD',
+        )
 
-    print(df_train.head(5))
-    print(df_train.dtypes)
+    print(emotions)
+    print(sentiments)
+    print(df_train_x.head(5))
+    print(df_train_x.dtypes)
+    print(df_train_y.head(5))
+    print(df_train_y.dtypes)
+
