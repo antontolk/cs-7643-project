d4522be6f3927736b65981e5bf651143dd9d3334
Anton Tolkachev <atolkachev3@gatech.edu>
2024-11-28 16:45:42 +1300

    1) Punctuation and signs removal; 2) Sentences stripping; 3) Lemmatization; 4) Stop words; 5) Max Top N speakers; 6) New parameter: max size of the sentence for BPE and Word tokenization

--- main.py
+++ main.py
@@ -25,9 +25,15 @@ if __name__ == '__main__':
         df_val=df_val,
         df_test=df_test,
         labels=settings.data_preprocessing.labels,
-        encode_speakers=settings.data_preprocessing.encode_speakers,
         utterance_processing=settings.data_preprocessing.utterance_processing,
+        remove_punc_signs=settings.data_preprocessing.remove_punc_signs,
+        strip=settings.data_preprocessing.strip,
+        lemmatization=settings.data_preprocessing.lemmatization,
         ngram=settings.data_preprocessing.ngram,
+        stop_words=settings.data_preprocessing.stop_words,
+        tokens_in_sentence=settings.data_preprocessing.tokens_in_sentence,
+        encode_speakers=settings.data_preprocessing.encode_speakers,
+        top_n_speakers=settings.data_preprocessing.top_n_speakers,
     )
 
     # Create the model
